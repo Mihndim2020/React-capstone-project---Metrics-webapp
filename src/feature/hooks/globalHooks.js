0a2a4dfcbabf46c95ec4a06bd53cc6d0d2993287
Mihndim Julius <mihndim2016@gmail.com>
2021-09-07 08:23:25 -0400

    Add global hocks function

---
+++ src/feature/hooks/globalHooks.js
@@ -0,0 +1,16 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getGlobalCovidAction } from '../redux/slices/covid';
+
+const globalHooks = () => {
+  const dispatch = useDispatch();
+  const global = useSelector((state) => state.global);
+
+  useEffect(() => {
+    if (!global.length) {
+      dispatch(getGlobalCovidAction());
+    }
+  }, []);
+};
+
+export default globalHooks;
--- src/feature/redux/slices/configStore.js
+++ src/feature/redux/slices/configStore.js
@@ -1,7 +1,15 @@
-import { createStore } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import covidReducer from './covid';
+
+const reducer = combineReducers({
+  covidReducer,
+});
 
 const store = createStore(
-
+  reducer,
+  applyMiddleware(thunk, logger),
 );
 
 export default store;
