4a317f464e266bca45ecaeb92483bac42965d25c
Mihndim Julius <mihndim2016@gmail.com>
2021-09-07 08:28:01 -0400

    Add function hook to update regional statistics

---
+++ src/feature/hooks/countryHooks.js
@@ -0,0 +1,16 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getCountryCovidAction } from '../redux/slices/covid';
+
+const countryHooks = () => {
+  const dispatch = useDispatch();
+  const country = useSelector((state) => state.country);
+
+  useEffect(() => {
+    if (!country.length) {
+      dispatch(getCountryCovidAction());
+    }
+  }, []);
+};
+
+export default countryHooks;
